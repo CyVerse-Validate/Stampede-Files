556f18a0d7397bd9a64d68c4c23923fb8476699b
James Gray <JamesG1021@users.noreply.github.com>
2016-07-26 11:25:50 -0400

    Updated GenSel_wrap.sh

--- GenSel/GenSel_wrap.sh
+++ GenSel/GenSel_wrap.sh
@@ -11,39 +11,41 @@ varGenotypic='${varGenotypic}'
 varResidual='${varResidual}'
 
 # No checks on these first two since they are always required 
-echo 'markerFileName	$geno_data' >> command_file.txt
-echo 'phenotypeFileName		$pheno_data' >> command_file.txt
+echo 'markerFileName    $geno_data' >> command_file.txt
+echo 'phenotypeFileName         $pheno_data' >> command_file.txt
 
 # Check if the others exist since they are all optional
 # May consider adding others depending on the full list of analysis types and arguments
-if [ ! -z $includeRange ];
-	echo 'includeRange	$includeRange' >> command_file.txt
+if [ ! -z $includeRange ]; then
+        echo 'includeRange      $includeRange' >> command_file.txt
 fi
 
-if [ ! -z $excludeFilename ];
-	echo 'excludeFileName	$excludeFilename' >> command_file.txt
+if [ ! -z $excludeFilename ]; then
+        echo 'excludeFileName   $excludeFilename' >> command_file.txt
 fi
 
-if [ ! -z $analysisType ];
-	echo 'analysisType	$analysisType' >> command_file.txt
+if [ ! -z $analysisType ]; then
+        echo 'analysisType      $analysisType' >> command_file.txt
 fi
 
-if [ ! -z $varGenotypic ];
-	echo 'varGenotypic	$varGenotypic' >> command_file.txt
+if [ ! -z $varGenotypic ]; then
+        echo 'varGenotypic      $varGenotypic' >> command_file.txt
 fi
 
-if [ ! -z $analysisType ];
-	echo 'varResidual	$varResidual' >> command_file.txt
-fi 
+if [ ! -z $analysisType ]; then
+        echo 'varResidual       $varResidual' >> command_file.txt
+fi
 
 tar -xvf GSrun4.55R.iPlant.tar
 export LD_LIBRARY_PATH=:`pwd`/GSrun4.55R/lib
 export PATH=$PATH:`pwd`/GSrun4.55R/bin
 export PATH=$PATH:`pwd`/GSrun4.55R/lib
-./bin/GenSel
-
+#chmod +x GSrun4.55R/bin/GenSel
+chmod +x GSrun4.55R/GSsetup
+chmod +x GSrun4.55R/bin/GenSel
+./GSrun4.55R/GSsetup
 
 
 # Run the program with the created command file
-chmod +x GSrun4.55R/bin/GenSel
-GenSel command_file.txt
+#chmod +x GSrun4.55R/bin/GenSel
+GSrun4.55R/bin/GenSel command_file.txt
